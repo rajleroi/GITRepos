d6311414aa3005c912c9150c442c225b4fac9f73
rajleroi <163645935+rajleroi@users.noreply.github.com>
2024-03-16 12:24:00 +0000

    Update GIT1.java

--- src/GIT1.java
+++ src/GIT1.java
@@ -1,6 +1,6 @@
 public class GIT {
 
     public static void main(String[] args) {
-        System.out.println("First GIT2 Repository");
+        System.out.println("First GIT2 Repository updated");
     }
 }
